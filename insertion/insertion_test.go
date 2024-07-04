79bddb9776dcb8c7b6515b262703aae73ec0083d
Maksym Nedvyha <imus663@gmail.com>
2024-07-04 23:32:54 +0300

    test: add tests for Insertion func

---
+++ insertion/insertion_test.go
@@ -0,0 +1,50 @@
+package insertion
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestInsertion_Sort_Int(t *testing.T) {
+	unsortedArr := []int{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5}
+	expectedArr := []int{1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9}
+
+	result := Insertion[int]{}.Sort(unsortedArr)
+
+	if !reflect.DeepEqual(expectedArr, result) {
+		t.Errorf("expected %v, got %v", expectedArr, result)
+	}
+}
+
+func TestInsertion_Sort_Int8(t *testing.T) {
+	unsortedArr := []int8{12, 34, -56, 78, 90, -12, 34, -56, 78, 90}
+	expectedArr := []int8{-56, -56, -12, 12, 34, 34, 78, 78, 90, 90}
+
+	result := Insertion[int8]{}.Sort(unsortedArr)
+
+	if !reflect.DeepEqual(expectedArr, result) {
+		t.Errorf("expected %v, got %v", expectedArr, result)
+	}
+}
+
+func TestInsertion_Sort_Uint32(t *testing.T) {
+	unsortedArr := []uint32{4294967295, 0, 123456789, 47, 643, 987654321, 456789123}
+	expectedArr := []uint32{0, 47, 643, 123456789, 456789123, 987654321, 4294967295}
+
+	result := Insertion[uint32]{}.Sort(unsortedArr)
+
+	if !reflect.DeepEqual(expectedArr, result) {
+		t.Errorf("expected %v, got %v", expectedArr, result)
+	}
+}
+
+func TestInsertion_Sort_Float64(t *testing.T) {
+	unsortedArr := []float64{3.14, 2.71, 1.61, 0.0, 1.41, 2.0, 3.14, 2.71, 1.61}
+	expectedArr := []float64{0.0, 1.41, 1.61, 1.61, 2.0, 2.71, 2.71, 3.14, 3.14}
+
+	result := Insertion[float64]{}.Sort(unsortedArr)
+
+	if !reflect.DeepEqual(expectedArr, result) {
+		t.Errorf("expected %v, got %v", expectedArr, result)
+	}
+}
